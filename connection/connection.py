66fd744593557eea20189bc5d8cad19782b47eb6
Zarate Carreno Jose Valentin <lc19160007@oaxaca.tecnm.mx>
2022-01-04 03:49:10 -0600

    🐛 FIX: connnect db for insert data

--- connection/connection.py
+++ connection/connection.py
@@ -15,3 +15,8 @@ class DataBase:
         self.cursor.execute("SELECT * FROM actividades")
         data = self.cursor.fetchall()
         return data
+    def setData(self,act1,act2,act3,act4,act5,act6,estado):
+        sql = "INSERT INTO actividades (actividad_1,actividad_2,actividad_3,actividad_4,actividad_5,actividad_6, estado) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+        val = (act1,act2,act3,act4,act5,act6,estado)
+        self.cursor.execute(sql, val)
+        self.connection.commit()
