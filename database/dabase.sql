5dfb453f426032dd68e8839c5f434c564ceae505
Zarate Carreno Jose Valentin <lc19160007@oaxaca.tecnm.mx>
2022-01-03 19:54:41 -0600

    💥 NEW: database sql

---
+++ database/dabase.sql
@@ -0,0 +1,10 @@
+CREATE TABLE actividades(  
+    id int NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'Primary Key',
+    actividad_1 varchar(255),
+    actividad_2 varchar(255),
+    actividad_3 varchar(255),
+    actividad_4 varchar(255),
+    actividad_5 varchar(255),
+    actividad_6 varchar(255),
+    estado varchar(255)
+) DEFAULT CHARSET UTF8 COMMENT '';
