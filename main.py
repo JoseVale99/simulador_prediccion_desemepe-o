e68d50c0e4d122fcbb9c0464623aba22b9dd4b01
Zarate Carreno Jose Valentin <lc19160007@oaxaca.tecnm.mx>
2022-01-04 03:48:25 -0600

    🐛 FIX: main preview

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import sys
 from view.index import Ui_MainWindow
 from controller.InsertarController import VistaInsertar
 from models.cargar_datos import CargarDatos
-
+from PyQt5.Qt import *
 class App(QMainWindow):
     def __init__(self):
         super(App, self).__init__()
@@ -18,6 +18,7 @@ class App(QMainWindow):
                   int((screen.height() - size.height())/2))
     # Eventos   
         self.index.btn_insert.clicked.connect(self.show_insert)
+        # self.index.btn_refrescar.clicked.connect(self.refresh)
     # Cargar tablas ----- 
         CargarDatos(self.index.tabla_datos,QTableWidgetItem)
         #  abrir ventada insertar datos
@@ -25,9 +26,12 @@ class App(QMainWindow):
         self.fom_insertar = QWidget() 
         self.view_insertar = VistaInsertar()
         self.view_insertar.vista_insertar.setupUi(self.fom_insertar)
-        self.fom_insertar.show()        
+        self.view_insertar.vista_insertar.btn_guardar.clicked.connect(lambda: self.view_insertar.insertData(self.index.tabla_datos,QTableWidgetItem))
+        self.fom_insertar.show() 
         
+   
                       
+   
     
         
 if __name__ == '__main__':
