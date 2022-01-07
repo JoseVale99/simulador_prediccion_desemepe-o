8e15e1cc847e165b0d3cf63c8345a12479d1c612
Zarate Carreno Jose Valentin <lc19160007@oaxaca.tecnm.mx>
2022-01-07 02:36:19 -0600

    ✅ Adding Tests:view predecir

--- controller/ProbabilidadController.py
+++ controller/ProbabilidadController.py
@@ -16,6 +16,7 @@ class VistaProbabilidad(QDialog):
         self.move(int((screen.width() - size.width())/2),
                   int((screen.height() - size.height())/2))
 
+    
     def insertTable(self,tabla, QTableWidgetItem,x,y,dato):
         tabla.setItem(x,y, QTableWidgetItem(str(dato)))
     
@@ -27,26 +28,47 @@ class VistaProbabilidad(QDialog):
         self.view_frecuencias.InsertDataFrecAct(4)
         self.view_frecuencias.InsertDataFrecAct(5)
         self.view_frecuencias.InsertDataFrecAct(6)
+        print(self.view_frecuencias.InsertDataFrecAct(6))
         # haciendo la tabla de probabilidades para cada actividad usando la suavidad de Laplace
-        for i in range(3):
-            for j in range(3):
-                a = self.view_frecuencias.getPosition(i,j)+1
-                b = self.view_frecuencias.getPosition(3,j)+3
-                res = float(a/b)
-                self.insertTable(self.vista_Probabilidad.tabla_pro_act1,QTableWidgetItem,i,j,res)
+        
+        self.view_frecuencias.getProbability(self.view_frecuencias.vista_frecuencias.tabla_fre_act1,
+                  self.vista_Probabilidad.tabla_pro_act1, QTableWidgetItem)
+        
         #  totales
-        self.insertTable(self.vista_Probabilidad.tabla_pro_act1,QTableWidgetItem,3,0,float(
-            self.view_frecuencias.getPosition(0,0)+self.view_frecuencias.getPosition(1,0)+
-            self.view_frecuencias.getPosition(2,0)
-        ))
-        self.insertTable(self.vista_Probabilidad.tabla_pro_act1,QTableWidgetItem,3,1,float(
-            self.view_frecuencias.getPosition(1,0)+self.view_frecuencias.getPosition(0,1)+
-            self.view_frecuencias.getPosition(1,1)
-        ))
-        self.insertTable(self.vista_Probabilidad.tabla_pro_act1,QTableWidgetItem,3,2,float(
-            self.view_frecuencias.getPosition(0,2)+self.view_frecuencias.getPosition(1,2)+
-            self.view_frecuencias.getPosition(2,2)
-        ))
+        self.insertTable(self.vista_Probabilidad.tabla_pro_act1,QTableWidgetItem,3,0,
+        float(self.view_frecuencias.getPosition( self.vista_Probabilidad.tabla_pro_act1,0,0)
+        +self.view_frecuencias.getPosition( self.vista_Probabilidad.tabla_pro_act1,1,0)+
+        self.view_frecuencias.getPosition(self.vista_Probabilidad.tabla_pro_act1,2,0)))
+        
+        self.insertTable(self.vista_Probabilidad.tabla_pro_act1,QTableWidgetItem,3,1,
+        float(self.view_frecuencias.getPosition( self.vista_Probabilidad.tabla_pro_act1,1,0)
+        +self.view_frecuencias.getPosition( self.vista_Probabilidad.tabla_pro_act1,0,1)+
+        self.view_frecuencias.getPosition(self.vista_Probabilidad.tabla_pro_act1,1,1)))
+        
+        self.insertTable(self.vista_Probabilidad.tabla_pro_act1,QTableWidgetItem,3,2,
+        float(self.view_frecuencias.getPosition( self.vista_Probabilidad.tabla_pro_act1,0,2)
+        +self.view_frecuencias.getPosition( self.vista_Probabilidad.tabla_pro_act1,1,2)+
+        self.view_frecuencias.getPosition(self.vista_Probabilidad.tabla_pro_act1,2,2)))
+        
+        
+        # tabla 2
+        self.view_frecuencias.getProbability(self.view_frecuencias.vista_frecuencias.tabla_fre_act2,
+                  self.vista_Probabilidad.tabla_pro_act2, QTableWidgetItem)
+        #  totales
+        self.insertTable(self.vista_Probabilidad.tabla_pro_act2,QTableWidgetItem,3,0,
+        float(self.view_frecuencias.getPosition( self.vista_Probabilidad.tabla_pro_act2,0,0)
+        +self.view_frecuencias.getPosition( self.vista_Probabilidad.tabla_pro_act2,1,0)+
+        self.view_frecuencias.getPosition(self.vista_Probabilidad.tabla_pro_act2,2,0)))
+        
+        self.insertTable(self.vista_Probabilidad.tabla_pro_act2,QTableWidgetItem,3,1,
+        float(self.view_frecuencias.getPosition( self.vista_Probabilidad.tabla_pro_act2,1,0)
+        +self.view_frecuencias.getPosition( self.vista_Probabilidad.tabla_pro_act2,0,1)+
+        self.view_frecuencias.getPosition(self.vista_Probabilidad.tabla_pro_act2,1,1)))
+        
+        self.insertTable(self.vista_Probabilidad.tabla_pro_act2,QTableWidgetItem,3,2,
+        float(self.view_frecuencias.getPosition( self.vista_Probabilidad.tabla_pro_act2,0,2)
+        +self.view_frecuencias.getPosition( self.vista_Probabilidad.tabla_pro_act2,1,2)+
+        self.view_frecuencias.getPosition(self.vista_Probabilidad.tabla_pro_act2,2,2)))
     
         
    
